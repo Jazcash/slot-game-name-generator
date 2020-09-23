703cfb0d7437fc2e456800fd50a9ae2418cb00cb
Jazcash <jaspercashmore@gmail.com>
2020-09-23 23:31:03 +0100

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,7 @@
 module.exports = {
     apps: [{
         name: "slot-game-name-generator",
-        script: "./node_modules/.bin/webpack-dev-server",
-        // env: {
-        //     NODE_ENV: "production",
-        // },
+        script: "npm",
+        args: "start"
     }]
 }
